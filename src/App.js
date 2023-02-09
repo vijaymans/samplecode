01a4315cba8b7260f45b51ddde35156d8285f807
vijaymans <84343836+vijaymans@users.noreply.github.com>
2023-02-09 22:32:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import FragmentDemo from "./FragmentDemo";
+import Table from "./table";
+
+//After version 17 there is no need to use import statement
+function App() 
+{
+  
+  return (
+    <div className="App">
+
+    {/* <FragmentDemo/> */}
+    <Table/>
+    </div>
+  );
+}
+
+export default App;
